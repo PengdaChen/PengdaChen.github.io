93bb0e90227c626400168525871cd65621f81f8c
giggity <2634662548@qq.com>
2026-09-16 10:46:30 +0800

    Add CountVid paper entry

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const papers = [
+  { model: 'CountVid', title: 'CountVid', image: '../images/countvid-architecture.png', aliases: ['countvid', 'video counting'] },
   { model: 'INSID3', title: 'INSID3', image: '../images/insid3-architecture.png', aliases: ['insid3', 'in-context segmentation'] },
   { model: 'DINO', title: 'DINO', image: '../images/dino-architecture.png' },
   { model: 'Vision Transformer (ViT)', title: 'Vision Transformer (ViT)', image: '../images/vit-architecture.png', aliases: ['vit', 'vision transformer'] },
